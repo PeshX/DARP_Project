2742bd97c70f1c45417cc064970477b6cf957db0
PeshX <marcopascarella187@gmail.com>
2024-06-13 18:17:10 +0200

    Added first implementation of mutation and crossover
    Co-authored-by: 01234nic <01234nic@users.noreply.github.com>

--- solver/genAlgo.py
+++ solver/genAlgo.py
@@ -1,6 +1,8 @@
-
+import random
+import numpy as np
 
 # FITNESS FUNCTION 
+
 def fitness(individual, graph) : 
     """
     @param individual := a solution, id est a list of lists of integers (list of transfers)
@@ -37,5 +39,51 @@ def fitness(individual, graph) :
     return res 
 
 
+# MUTATION
 
+def IntegerRandMutation(chromosome, p, passengers_list):
+
+    for i in range(0,len(chromosome)-1):
+        if p > random.uniform(0.0,1.0):
+            chromosome[i] = random.randint(0, max(passengers_list))
+
+    return chromosome
+
+
+# RECOMBINATION
+
+def LineRecombIntegers(individuals, p, passengers_list):
+
+    alpha = random.uniform(-p, 1+p)
+    beta = random.uniform(-p, 1+p)
+
+    len_short_chr = len(min(individuals,key=len))
+
+    sx_half_long_chr = np.floor(len(min(individuals,key=len))/2)
+    low_index = random.randint(0, sx_half_long_chr)
+
+    while (low_index+len_short_chr < len(max(individuals,key=len)-1)):
+                    
+           low_index = random.randint(0, sx_half_long_chr)
+
+    if len(individuals[1]) > len(individuals[2]):
+        long_index = 1
+        short_index = 2
+
+    else:
+        long_index = 2
+        short_index = 1
+
+
+    for i in range(0,len(min(individuals,key=len))-1):
+
+
+        t = alpha * individuals[long_index][i+low_index] +  beta * individuals[short_index][i]
+        s = alpha * individuals[short_index][i] +  beta * individuals[long_index][i+low_index]
+
+        while((np.floor(t+0.5) in passengers_list) and (np.floor(s+0.5) in passengers_list)):
+            individuals[1][i] = np.floor(t+0.5)
+            individuals[2][i] = np.floor(s+0.5)
+
+    return individuals
 
